ffd8e6c3d7464ba64407b45d358e0d2a5c3c7cc9
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-06-20 23:46:54 +0900

    editorの設定を変更した

---
+++ Assets/Editor/SpringCapsuleColliderInspector.cs
@@ -0,0 +1,27 @@
+﻿#if UNITY_EDITOR
+using System.Linq;
+using UnityEditor;
+
+namespace Unity.Animations.SpringBones
+{
+    [CustomEditor(typeof(SpringCapsuleCollider))]
+    [CanEditMultipleObjects]
+    public class SpringCapsuleColliderInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            if (boneSelector == null)
+            {
+                boneSelector = SpringColliderBoneSelector.Create<SpringCapsuleCollider>(
+                    targets, (bone, colliders) => bone.capsuleColliders.Any(collider => colliders.Contains(collider)));
+            }
+            boneSelector.ShowInspector();
+        }
+
+        // private
+
+        private SpringColliderBoneSelector boneSelector;
+    }
+}
+#endif
