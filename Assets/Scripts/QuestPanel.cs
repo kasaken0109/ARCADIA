35cab11c5d18e1a85663adaf0ffbb450d6605701
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-07-14 17:18:37 +0900

    余分な処理を削除した

--- Assets/Scripts/QuestPanel.cs
+++ Assets/Scripts/QuestPanel.cs
@@ -6,19 +6,19 @@ using UnityEngine.UI;
 public class QuestPanel : MonoBehaviour
 {  
     [SerializeField]
-    private Image m_questImage = null;
+    Image m_questImage = null;
     [SerializeField]
-    private Image m_questTitleImage = null;
+    Image m_questTitleImage = null;
     [SerializeField]
-    private Image m_questBackGroundImage = null;
+    Image m_questBackGroundImage = null;
     [SerializeField]
-    private Text m_questName = null;
+    Text m_questName = null;
     [SerializeField]
-    private Text m_questText = null;
+    Text m_questText = null;
 
     string m_loadSceneName = null;
 
-    private Quest m_quest = null;
+    Quest m_quest = null;
     public void SetQuest(Quest quest)
     {
         m_quest = quest;
@@ -29,17 +29,9 @@ public class QuestPanel : MonoBehaviour
         m_questName.text = m_quest.QuestName();
         m_questText.text = m_quest.QuestText();
     }
-    private void Update()
-    {
-        if (!m_quest) Debug.LogError("QuestDataがありません！");
-        m_questImage.sprite = m_quest.QuestImage();
-        m_questTitleImage.color = m_quest.QuestColor();
-        m_questBackGroundImage.sprite = m_quest.QuestBackGroundImage();
-        m_loadSceneName = m_quest.LoadSceneName();
-        m_questName.text = m_quest.QuestName();
-        m_questText.text = m_quest.QuestText();
-    }
-
+    /// <summary>
+    /// 選択されたクエストのシーンをロード
+    /// </summary>
     public void LoadQuest()
     {
         SceneLoader.Instance.SceneLoad(m_loadSceneName);
